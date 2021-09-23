f5cfa46784ed584581afdd824a4389c82f9b14c5
nkzren <renan.nakazawa0@gmail.com>
2021-09-22 21:23:38 -0300

    wip: adiciona busca de locais

--- components/local/Card.vue
+++ components/local/Card.vue
@@ -34,9 +34,9 @@
     },
     methods: {
       listAccessibility() {
-        const keys = Object.keys(this.accessibility).filter(key => this.accessibility[key] === true);
+        // const keys = Object.keys(this.accessibility).filter(key => this.accessibility[key] === true);
 
-        this.availableAccessibility = keys;
+        this.availableAccessibility = {};
       }
     },
     created() {
